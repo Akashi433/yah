11a6f0e46d6be3d800c4d869af12f3666276372b
Akashi433 <tohkarinka464@gmail.com>
2024-08-18 09:23:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const apikeyRoute = require('./routes/apikey.route');
+const apikeyRoute = require('./routes/apikey.routes');
 
 app.use(express.json());
 app.use('/api', apikeyRoute);
