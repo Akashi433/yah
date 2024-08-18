9d5c11a149163da30d17636fff14d7e0628e65a8
Akashi433 <tohkarinka464@gmail.com>
2024-08-18 10:27:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const apiKeys = require('./routes/apiKeys');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const apiRouter = require('./router/api');
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://otsuka:BocahGila@cluster0.ilka0bz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+app.use('/api', apiRouter);
 
-app.use(bodyParser.json());
-app.use('/api', apiKeys);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
