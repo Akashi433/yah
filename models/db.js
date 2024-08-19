a9536380c15bb52380414878ee5c29c741c45e19
Akashi433 <tohkarinka464@gmail.com>
2024-08-19 16:39:24 +0700

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
 const apiKeySchema = new mongoose.Schema({
-  key: { type: String, required: true, unique: true },
-  isPremium: { type: Boolean, default: false },
-  usageCount: { type: Number, default: 0 },
-  limit: { type: Number, default: 100 }, // default limit untuk free
-  createdAt: { type: Date, default: Date.now },
+    key: { type: String, required: true, unique: true },
+    isPremium: { type: Boolean, default: false },
+    usageCount: { type: Number, default: 0 },
+    limit: { type: Number, default: 100 }, // limit untuk free
+    lastReset: { type: Date, default: Date.now }
 });
 
-const ApiKey = mongoose.model('ApiKey', apiKeySchema);
-module.exports = ApiKey;
+module.exports = mongoose.model('ApiKey', apiKeySchema);
