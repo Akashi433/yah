926f74c641b1233f946ca66eb378086580b9ba57
Akashi433 <tohkarinka464@gmail.com>
2024-08-19 16:15:17 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const apiRouter = require('./router/api');
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://otsuka:BocahGila@cluster0.ilka0bz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://otsuka:BocahGila@cluster0.ilka0bz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.use('/api', apiRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
