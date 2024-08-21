717b398b1c246521774ad3ec913347837650dac1
Akashi433 <tohkarinka464@gmail.com>
2024-08-21 09:05:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch(err => console.log(err));
 
 app.use('/api', apiRouter);
-app.use('/main', mainRouter);
+app.use('/', mainRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
